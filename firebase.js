dbebaba22745b370a35231ec80edfcb27123c056
JonnaI89 <114297180+JonnaI89@users.noreply.github.com>
2025-06-03 19:29:39 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase, ref, onValue, set, update } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getDatabase, ref, onValue, set, update, get } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDx-zN7rKyDivezJY_jpdnyB1P6dvSI9WI",
   authDomain: "live-score-f4f27.firebaseapp.com",
   databaseURL: "https://live-score-f4f27-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "live-score-f4f27",
-  storageBucket: "live-score-f4f27.firebasestorage.app",
+  storageBucket: "live-score-f4f27.appspot.com", // <-- FIXED HERE
   messagingSenderId: "542532741836",
   appId: "1:542532741836:web:871520a37565342d187069",
   measurementId: "G-5L4K9TX51C"
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, onValue, set, update };
+export { db, ref, onValue, set, update, get };
