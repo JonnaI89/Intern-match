8d2535b2d838a4f89dfbe9ba4da214dad399965f
JonnaI89 <114297180+JonnaI89@users.noreply.github.com>
2025-06-03 19:11:44 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // admin.js
-import { db, ref, onValue, set, update, get } from './firebase.js';
+import { getDatabase, ref, onValue, set, update, get } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 // HTML elements
 const teamANameInput = document.getElementById('teamANameInput');
